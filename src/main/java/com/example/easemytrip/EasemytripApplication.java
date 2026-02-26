6ccf9705af8804ac6451b42c7fca879361cce5c3
Shivraj Akuskar <shivrajakuskar7@gmail.com>
2026-02-26 06:42:01 +0530

    chnages in ignore file

---
+++ src/main/java/com/example/easemytrip/EasemytripApplication.java
@@ -0,0 +1,13 @@
+package com.example.easemytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasemytripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasemytripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easemytrip/EasemytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easemytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasemytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
